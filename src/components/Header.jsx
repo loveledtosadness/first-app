830bd123b390c1becbed9834b01a0507a65b877e
Egor Chubukin <Egor Chubukin>
2023-12-04 23:24:33 +0300

    modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Header.jsx 	new file:   src/components/Navig.jsx 	new file:   src/components/Profile.jsx
    Changes not staged for commit:
    	modified:   src/App.css
    	modified:   src/App.js
    	deleted:    src/components/Header.jsx
    	deleted:    src/components/Navig.jsx
    	deleted:    src/components/Profile.jsx
    
    Untracked files:
    	src/components/Header/
    	src/components/Navig/
    	src/components/Profile/

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <header className="Penis">
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Polygon_Blockchain_Matic_Logo.svg/1200px-Polygon_Blockchain_Matic_Logo.svg.png' />
+      </header> 
+    )
+}
+
+
+export default Header;
---
+++ src/components/Navig.jsx
@@ -0,0 +1,23 @@
+const Navig = () => {
+    return (
+        <nav className="Booba">
+        <div>
+        <a href="#">Profile</a>
+        </div>
+        <div>
+        <a href="#">Messages</a>
+        </div>
+        <div>
+        <a href="#">News</a>
+        </div>
+        <div>
+        <a href="#">Music</a>
+        </div>
+        <div>
+        <a href="#">Settings</a>
+        </div>
+      </nav>
+    )
+}
+
+export default Navig;
